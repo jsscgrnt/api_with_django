5bec4f4a579711069754ac4ab76b66b688bbb818
Jessica <jessicagerente@gmail.com>
2022-01-16 18:21:46 -0300

    change content on main.py

--- crypto/data_preparation/source/main.py
+++ crypto/data_preparation/source/main.py
@@ -1,25 +1,24 @@
+import requests
 from matrix_generation import *
 
-chave = 'MSZHWQLFNREIQUQLAMDGXOKAP'
-palavra = 'JESSICA'
+host = 'http://127.0.0.1:8000'
+service = '/crypto/'
+
+chave = "MSZHWQLFNREIQUQLAMDGXOKAP"
+palavra = "JESSICA"
 
 teste = generate_final_order().sort_words(chave)
 teste = ''.join(teste)
-# print(teste)
-# print('\n')
-
 teste2 = cleaning_data(teste).change_y_for_i()
-# print(teste2)
-# print('\n')
-
 teste3 = cleaning_data(teste2).ignore_duplicated()
-# print(teste3)
-# print(len(teste3))
-# print('\n')
-
 teste4 = generate_final_order().sorted_letter_matrix(teste3)
-print(teste4)
-print('\n')
-
 teste5 = generate_final_order().key_position_in_alphabet_matrix(letter_matrix= teste4, encrypto_word=palavra)
-print(teste5)
+
+palavra_encriptada = teste5
+
+pload = {"chave":chave,"palavra":palavra,"palavra_encriptada":palavra_encriptada}
+r = requests.post(f'{host}{service}', data = pload)
+print(r)
+
+r = requests.get(f'{host}{service}')
+print(r.text)
